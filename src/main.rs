86d4f69d9f39250e39749b1c09ea793f8c1d4852
shahzad akhtar <shahzad@mozark.ai>
2024-04-04 13:14:04 +0530

    ready.set,go!

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+extern crate log;
+use env_logger;
+use log::debug;
+use log::error;
+use log::info;
+use log::warn;
+//use log::{debug, error, info};
+use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    env_logger::init();
 
     // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let port = 4221;
+    let listener = TcpListener::bind(format!("127.0.0.1:{port}")).unwrap();
+    println!("listening on port {port}");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
